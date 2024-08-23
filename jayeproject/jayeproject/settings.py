06b51dd8674b68ac7cf6dc5c0bd95811f3aadca3
Emmanuel1472 <114288711+Emmanuel1472@users.noreply.github.com>
2024-08-23 23:38:24 +0100

    staticfiles update

--- jayeproject/jayeproject/settings.py
+++ jayeproject/jayeproject/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,9 +121,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/ 'static']
 
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR/'media'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

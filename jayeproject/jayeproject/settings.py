ff458d9b20aa0ebe5f08f9f6039fada7235001ba
Emmanuel1472 <114288711+Emmanuel1472@users.noreply.github.com>
2024-08-29 14:55:54 +0100

    whitenoise.storage removed

--- jayeproject/jayeproject/settings.py
+++ jayeproject/jayeproject/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'jayeproject.urls'

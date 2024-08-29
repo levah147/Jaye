16f5c8ac3ae5f5da7f8d146426c612d4ca65ea5f
Emmanuel1472 <114288711+Emmanuel1472@users.noreply.github.com>
2024-08-29 14:57:32 +0100

    whitenoise.storage removed2

--- jayeproject/jayeproject/settings.py
+++ jayeproject/jayeproject/settings.py
@@ -127,7 +127,6 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR/'media'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
